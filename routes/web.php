4d03c4905dc9414b1d1e95b8c251238e5d3f6a3b
leetaehee <lastride25@naver.com>
2020-02-05 14:32:23 +0900

    사용자인증

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,28 @@
 
 Route::get('/', 'WelcomeController@index');
 Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', function(){
+	$credentials = [
+		'email'=> 'john@example.com',
+		'password'=> 'pass1word'
+	];
+
+	if (! auth()->attempt($credentials) ) {
+		return '로그인 정보가 정확하지 않습니다.';
+	}
+
+	return redirect('protected');
+})->name('auth_login');
+
+Route::get('protected', ['middleware' => 'auth', function () { 
+	dump(session()->all());
+
+	return '어서오세요' . auth()->user()->name;
+}]);
+
+Route::get('auth/logout', function(){
+	auth()->logout();
+
+	return '또 봐요~';
+});
