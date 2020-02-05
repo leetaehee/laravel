62300244009a17a7e82c9e0b73b8181cb600f62d
leetaehee <lastride25@naver.com>
2020-02-05 15:58:10 +0900

    엘로퀀트 ORM

--- database/migrations/2020_02_05_055951_create_articles_table.php
+++ database/migrations/2020_02_05_055951_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+			$table->integer('user_id')->unsigned()->index();
+			$table->string('title');
+			$table->text('content');
+			$table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('users')
+				->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
