87508c9e020cead1696fb6e0fb6cc7b8ccd96e3a
leetaehee <lastride25@naver.com>
2020-02-27 22:27:22 +0900

    이메일 보내기

--- routes/web.php
+++ routes/web.php
@@ -43,14 +43,16 @@ Route::get('mail', function(){
    $article = \App\Article::with('user')->find(1);
 
 	return Mail::send(
-       'emails.articles.created',
-       compact('article'),
-       function ($message) use ($article) {
+	    ['text'=> 'emails.articles.created-text'],
+        //'emails.articles.created',
+        compact('article'),
+        function ($message) use ($article) {
            $message->from('lastride25@kevinlab.com', '케빈랩_이태희주임');
 		   $message->to(['lastride25@naver.com','ceman08071039@gmail.com']);
 		   $message->subject('[New] 새 글이 등록되었습니다-', $article->title);
 		   $message->cc('lastride25@icloud.com');
-       }
-   );
+		   $message->attach(storage_path('infocean.png'));
+        }
+    );
 
 });
