31b19fec8bd191f7952716c7a3dd92774b93cd10
leetaehee <lastride25@naver.com>
2020-03-15 16:20:08 +0900

    사용자 인증 재구성 초기작업

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,57 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+/* 메인 */
 Route::get('/', 'WelcomeController@index');
-Route::resource('articles', 'ArticlesController');
 
-Route::get('auth/login', function(){
-	$credentials = [
-		'email'=> 'john@example.com',
-		'password'=> 'pass1word'
-	];
+/* 사용자가입*/
+Route::get('auth/register', [
+    'as' => 'users.create',
+    'users' => 'UsersController@create'
+]);
 
-	if (! auth()->attempt($credentials) ) {
-		return '로그인 정보가 정확하지 않습니다.';
-	}
+Route::post('auth/register', [
+    'as' => 'users.store',
+    'users' => 'UsersController@store'
+]);
 
-	return redirect('protected');
-})->name('auth_login');
+Route::get('auth/confirm/{code}', [
+    'as' => 'users.confirm',
+    'users' => 'UsersController@confirm'
+]);
 
-Route::get('protected', ['middleware' => 'auth', function () { 
-	dump(session()->all());
+/* 사용자 인증 */
+Route::get('auth/login', [
+    'as' => 'sessions.create',
+    'users' => 'SessionsController@create'
+]);
 
-	return '어서오세요' . auth()->user()->name;
-}]);
+Route::post('auth/login', [
+    'as' => 'sessions.store',
+    'users' => 'SessionsController@store'
+]);
 
-Route::get('auth/logout', function(){
-	auth()->logout();
+Route::get('auth/logout', [
+   'as' => 'sessions.destory',
+   'users' => 'SessionsController@destory'
+]);
 
-	return '또 봐요~';
-});
+/* 비밀번호 초기화 */
+Route::get('auth/remind', [
+    'as' => 'remind.create',
+    'users' => 'PasswordsController@getRemind'
+]);
 
-Route::get('mail', function(){
-   $article = \App\Article::with('user')->find(1);
+Route::post('auth/remind', [
+    'as' => 'remind.store',
+    'users' => 'PasswordsController@postRemind'
+]);
 
-	return Mail::send(
-	    //['text'=> 'emails.articles.created-text'],
-        'emails.articles.created',
-        compact('article'),
-        function ($message) use ($article) {
-           $message->from('lastride25@kevinlab.com', '케빈랩_이태희주임');
-		   $message->to(['lastride25@naver.com','ceman08071039@gmail.com']);
-		   $message->subject('[New] 새 글이 등록되었습니다-', $article->title);
-		   $message->cc('lastride25@icloud.com');
-		   $message->attach(storage_path('infocean.png'));
-        }
-    );
+Route::get('auth/reset/{token}', [
+    'as' => 'reset.create',
+    'users' => 'PasswordsController@getReset'
+]);
 
-});
+Route::post('auth/reset', [
+    'as' => 'reset.store',
+    'users' => 'PasswordsController@postReset'
+]);
